17a57d32e532e3734c5c73fb939af015e422f513
Dmitry029 <kdp_029@tut.by>
2026-06-04 11:12:04 +0300

    Добавил два теста для TextBoxTests

--- src/test/java/tests/TextBoxTests.java
+++ src/test/java/tests/TextBoxTests.java
@@ -1,27 +1,47 @@
 package tests;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
 
 public class TextBoxTests extends BaseTest {
 
+    @BeforeEach
+    public void beforeEach() {
+        Selenide.open("/text-box");
+    }
+
     @Test
-    void successfulFillTest() {
-        open("/text-box");
+    void successFulFillTest() {
 
         $("[id=userName]").val("Alex Black");
         $("[id=userEmail]").val("alex@black.com");
         $("[id=currentAddress]").val("first address 1");
         $("[id=permanentAddress]").val("second address 2");
-        $("[id=submit]").click();
+        $("#submit").scrollTo().click();
 
         $("#name").shouldHave(text("Alex Black"));
         $("[id=output] [id=email]").shouldHave(text("alex@black.com"));
         $("[id=output] [id=currentAddress]").shouldHave(text("first address 1"));
         $("[id=output] [id=permanentAddress]").shouldHave(text("second address 2"));
+    }
+
+    @Test
+    void oneFieldFillTest() {
+
+        $("[id=userName]").val("Alex Black");
+        $("#submit").scrollTo().click();
+        $("#name").shouldHave(text("Alex Black"));
+    }
+
+    @Test
+    void negativeNoneOfTheFormFieldsAreFilledInTest() {
+        $("#submit").scrollTo().click();
+        $("#output").shouldNotBe(Condition.visible);
 
     }
 }
