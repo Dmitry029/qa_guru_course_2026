a1d9f44984843c7944df979ef2623ad7d4e59d1c
Dmitry029 <kdp.029029@gmail.com>
2026-08-03 17:43:09 +0300

    Добавил запуск на Selenoid.

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -16,8 +16,10 @@ public class BaseTest {
     @BeforeAll
     public static void setUp() {
         Configuration.browserSize = "1920*1080";
-        Configuration.browser = "chrome";
+        //Configuration.browser = "chrome";
+        Configuration.browserVersion = "149.0";
         Configuration.baseUrl = "https://demoqa.com";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 
     @AfterEach
