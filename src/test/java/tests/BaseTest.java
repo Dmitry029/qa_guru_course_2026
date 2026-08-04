b48be0cac0497b8d5bbcc2f9a377bef13966a6df
Dmitry029 <kdp.029029@gmail.com>
2026-08-04 13:36:59 +0300

    Усовершенствованный отчет

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -16,10 +16,10 @@ public class BaseTest {
     @BeforeAll
     public static void setUp() {
         Configuration.browserSize = "1920*1080";
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         //Configuration.browser = "chrome";
         //Configuration.browserVersion = "149.0";
-        Configuration.baseUrl = "https://demoqa.com";
-        //Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 
     @AfterEach
