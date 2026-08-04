efa4fac49499c772a67ab10d13da9f7c75ef11b1
Dmitry029 <kdp.029029@gmail.com>
2026-08-04 17:11:37 +0300

    Добавил Attachments.

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,8 +1,12 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import pages.RegistrationPage;
 import pages.components.ResultOfFillingOutTheFormComponent;
 
@@ -22,8 +26,18 @@ public class BaseTest {
         //Configuration.browserVersion = "149.0";
     }
 
+    @BeforeEach
+    void addListener() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+
     @AfterEach
-    public void tearDown() {
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
         closeWebDriver();
     }
+
 }
